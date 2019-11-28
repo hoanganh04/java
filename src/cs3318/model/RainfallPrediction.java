de9f431152616a72276a984baec8a6f35ff2be5a
Hoang Anh <hoanganh.hoang44@gmail.com>
2019-11-28 17:39:18 +0000

    Updated NullSourceException

--- src/cs3318/model/RainfallPrediction.java
+++ src/cs3318/model/RainfallPrediction.java
@@ -1,6 +1,7 @@
 package cs3318.model;
 
 import cs3318.datastore.RainfallDataSource;
+import cs3318.exceptions.NullSourceException;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -8,10 +9,10 @@ import java.util.Objects;
 public abstract class RainfallPrediction {
     protected RainfallDataSource data;
 
-    public RainfallPrediction(RainfallDataSource source) {
+    public RainfallPrediction(RainfallDataSource source) throws NullSourceException {
         Objects.requireNonNull(source);
         this.data = source;
     }
 
-    public abstract Double predict(LocalDate date);
+    public abstract Double predict(LocalDate date) throws NullSourceException;
 }

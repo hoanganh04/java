3cf073f97b590c8af6daea758e4877a939e2870a
Hoang Anh <hoanganh.hoang44@gmail.com>
2019-11-28 17:38:52 +0000

    Updated NullSourceException

--- src/cs3318/model/AveragingPrediction.java
+++ src/cs3318/model/AveragingPrediction.java
@@ -1,6 +1,7 @@
 package cs3318.model;
 
 import cs3318.datastore.RainfallDataSource;
+import cs3318.exceptions.NullSourceException;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -9,49 +10,51 @@ import java.util.Arrays;
 public class AveragingPrediction extends RainfallPrediction {
     private int numberOfSamples = 3;
 
-    public AveragingPrediction(RainfallDataSource source) {
+    public AveragingPrediction(RainfallDataSource source) throws NullSourceException {
         super(source);
     }
 
     public void setNumberOfSamples(int n) {
         this.numberOfSamples = n;
     }
-//Return number of samples.
+
+    // Return number of samples.
     public int getNumberOfSamples(int n) {
         return numberOfSamples;
     }
 
     @Override
-    public Double predict(LocalDate date) {
-        Double [] predictionData = new Double[this.numberOfSamples];
+    public Double predict(LocalDate date) throws NullSourceException {
+        Double[] predictionData = new Double[this.numberOfSamples];
         Double data_check;
-        boolean array_check=true;
+        boolean array_check = true;
 
         LocalDate sampleYear = date.minus(1, ChronoUnit.YEARS);
 
         for (int i = 0; i < this.numberOfSamples; i += 1) {
-            Integer indexOfDate = data.getRecordingDates().indexOf(sampleYear);
-//Prediction Data elements validation.
             try {
-                data_check = data.getPrecipitation().get(i);
-                predictionData[i]=data_check;
-                sampleYear = date.minus(1, ChronoUnit.YEARS);
-            }
-            catch (NumberFormatException e) {
-                
-                array_check=false;
-                System.out.println(e);
-                break;
+                Integer indexOfDate = data.getRecordingDates().indexOf(sampleYear);
+                // Prediction Data elements validation.
+                try {
+                    data_check = data.getPrecipitation().get(i);
+                    predictionData[i] = data_check;
+                    sampleYear = date.minus(1, ChronoUnit.YEARS);
+                } catch (NumberFormatException e) {
+
+                    array_check = false;
+                    System.out.println(e);
+                    break;
+                }
+            } catch (NullPointerException e) {
+                throw new NullSourceException("");
             }
         }
-// Prediction Data array validation.
-        if (array_check){
+        // Prediction Data array validation.
+        if (array_check) {
             return Arrays.stream(predictionData).mapToDouble(Double::doubleValue).average().orElse(Double.NaN);
-        }
-        else {
+        } else {
             return null;
         }
+
     }
-
-
 }

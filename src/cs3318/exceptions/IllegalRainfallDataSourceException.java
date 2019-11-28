dab374d67c26873b27fd1ab83fa2cac0bbc1ed91
Hoang Anh <hoanganh.hoang44@gmail.com>
2019-11-28 16:50:23 +0000

    Add exception for FileNotFound error

--- src/cs3318/exceptions/IllegalRainfallDataSourceException.java
+++ src/cs3318/exceptions/IllegalRainfallDataSourceException.java
@@ -1,4 +1,8 @@
 package cs3318.exceptions;
 
+//Exception for FileNotFound Exception
 public class IllegalRainfallDataSourceException extends Exception {
+    public IllegalRainfallDataSourceException(){
+        System.out.println("File not found");
+    }
 }

de2c1a9c4486bc55ca407861ddbaf12491227e6d
Hoang Anh <hoanganh.hoang44@gmail.com>
2019-11-28 17:40:08 +0000

    NullSourceException for RainfallPredictions

---
+++ src/cs3318/exceptions/NullSourceException.java
@@ -0,0 +1,8 @@
+package cs3318.exceptions;
+
+//Exception NullSource Exception
+public class NullSourceException extends Exception {
+    public NullSourceException(String mess){
+        super(mess);
+    }
+}

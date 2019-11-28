aae1b806fb16ed31050b1c532fba1c4726a5c512
Hoang Anh <hoanganh.hoang44@gmail.com>
2019-11-28 17:42:22 +0000

    Updated IllegalRainFallDataSourceException

--- src/cs3318/exceptions/IllegalRainfallDataSourceException.java
+++ src/cs3318/exceptions/IllegalRainfallDataSourceException.java
@@ -2,7 +2,7 @@ package cs3318.exceptions;
 
 //Exception for FileNotFound Exception
 public class IllegalRainfallDataSourceException extends Exception {
-    public IllegalRainfallDataSourceException(){
-        System.out.println("File not found");
+    public IllegalRainfallDataSourceException(String mess){
+        super(mess);
     }
 }
